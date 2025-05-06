fa247693441cd0ecaf56f737e005c41a6c668e10
MalteMederacke <malte.mederacke@bsse.ethz.ch>
2025-05-06 16:32:36 +0200

    streaming example

--- scripts/distance_transform_label.py
+++ scripts/distance_transform_label.py
@@ -1,7 +1,7 @@
 import dask.array as da  # noqa
+from dask.diagnostics import ProgressBar
 import numpy as np
 import pyclesperanto as cle
-import h5py
 from skeleplex.preprocessing.distance_transform import normalized_distance_transform
 
 
@@ -9,7 +9,7 @@ from skeleplex.preprocessing.distance_transform import normalized_distance_trans
 
 if __name__ == "__main__":
     img = np.zeros((100, 100, 100), dtype=np.uint8)
-    img[30:70, 200:40, 200:70] = 1
+    img[30:70, 20:40, 20:70] = 1
 
     img = da.from_array(img, chunks=(20, 20, 20))
 
@@ -20,14 +20,12 @@ if __name__ == "__main__":
 
     img_normalized = normalized_distance_transform(img, depth=10, min_ball_radius=6)
 
-    # computed image
-    img_normalized_np = img_normalized.compute()
-
-    # store as input for nn
-    with h5py.File("test.h5", "w") as f:
-        f.create_dataset(
-            "normalized_vector_background_image",
-            data=img_normalized_np,
+    with ProgressBar():
+        da.to_hdf5(
+            "test.h5",
+            {
+                "normalized_vector_background_image": img_normalized,
+                "labels": img,
+            },
             compression="gzip",
         )
-        f.create_dataset("labels", data=img, compression="gzip")
