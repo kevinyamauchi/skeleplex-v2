5dd22abe5519c4374cf2795f834967672e0f8716
MalteMederacke <malte.mederacke@bsse.ethz.ch>
2025-06-20 13:01:30 +0200

    remove woopsis from test

--- tests/graph/test_sample.py
+++ tests/graph/test_sample.py
@@ -5,22 +5,6 @@ from skeleplex.graph.sample import (    generate_2d_grid,
 import numpy as np
 import dask.array as da
 
-
-
-volume = np.zeros((10, 10, 10), dtype=np.float32)
-volume[2:8, 2:8, 2:8] = 1
-volume = da.from_array(volume, chunks=(5, 5, 5))
-sampling_grid = generate_2d_grid((12,12),(1,1))
-sampling_grid += 5
-
-slice = sample_volume_at_coordinates_lazy(volume, sampling_grid,
-                                          interpolation_order=1,
-                                          fill_value = 5)
-assert slice[5,5] == 1
-assert slice[1,5] == 0
-assert slice[0,5] == 5 
-
-
 def test_sample_volume_at_coordinates():
     """Test sampling a volume at specific coordinates."""
     # Create a simple 3D volume
