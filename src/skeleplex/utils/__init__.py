9b76d31f373a59f66f9c70f222a73370748e8f77
Kevin <kevin.yamauchi@gmail.com>
2025-11-03 11:25:06 +0100

    update import

--- src/skeleplex/utils/__init__.py
+++ src/skeleplex/utils/__init__.py
@@ -1,5 +1,6 @@
 """Utilities for operating on the skeleton."""
 
+from skeleplex.utils._chunked import iteratively_process_chunks_3d
 from skeleplex.utils._geometry import line_segments_in_aabb, points_in_aabb
 
-__all__ = ["line_segments_in_aabb", "points_in_aabb"]
+__all__ = ["iteratively_process_chunks_3d", "line_segments_in_aabb", "points_in_aabb"]
