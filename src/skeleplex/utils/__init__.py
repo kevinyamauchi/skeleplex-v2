8b89625dcf6e2acaca53a3e8eaaee813ae39f925
Kevin <kevin.yamauchi@gmail.com>
2025-12-10 17:26:39 +0100

    add tta

--- src/skeleplex/utils/__init__.py
+++ src/skeleplex/utils/__init__.py
@@ -11,6 +11,22 @@ from skeleplex.utils._inference_slurm import (
     infer_on_chunk,
     initialize_parallel_inference,
 )
+from skeleplex.utils._tta import TTAWrapper
+from skeleplex.utils._tta_augmentations import (
+    FlipX,
+    FlipY,
+    FlipZ,
+    Identity,
+    Rot90YX,
+    Rot90ZX,
+    Rot90ZY,
+    Rot180YX,
+    Rot180ZX,
+    Rot180ZY,
+    Rot270YX,
+    Rot270ZX,
+    Rot270ZY,
+)
 
 __all__ = [
     "iteratively_process_chunks_3d",
@@ -21,4 +37,18 @@ __all__ = [
     "initialize_parallel_inference",
     "infer_on_chunk",
     "build_sbatch_command",
+    "Identity",
+    "FlipZ",
+    "FlipY",
+    "FlipX",
+    "Rot90ZY",
+    "Rot180ZY",
+    "Rot270ZY",
+    "Rot90ZX",
+    "Rot180ZX",
+    "Rot270ZX",
+    "Rot90YX",
+    "Rot180YX",
+    "Rot270YX",
+    "TTAWrapper",
 ]
